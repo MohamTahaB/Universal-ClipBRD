9c591df299efad90449b8810b4581c5b2558299b
MohamedTahaBayoumi <bayoumimt@gmail.com>
2023-01-22 19:13:47 +0100

    First commit : set environment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+import Text from "./Text.js";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Welcome to the universal ClipBoard !</h1>
+      </header>
+        <Text></Text>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Text.js
@@ -0,0 +1,13 @@
+import "./Text.css"
+
+function Text() {
+    return (
+        <div className={"textZone"}>
+            <form>
+                <input type={"text"} id={"text"} name={"text"}/>
+            </form>
+        </div>
+    ) ;
+}
+
+export default Text
